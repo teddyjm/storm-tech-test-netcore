5541d411a43ce459ddb398c5433b240aaa99f336
jacekmis <jacek@ministerstwoit.pl>
2020-03-31 17:02:55 +0200

    TASK #8 gravatar profiles are now integrated

---
+++ Todo.Tests/GravatarService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Services;
+using Xunit;
+
+namespace Todo.Tests
+{
+    public class GravatarService
+    {
+        [Fact]
+        public async Task GetUserProfile_ValidProfile_ShouldReturnData()
+        {
+            var validEmail = "beau@automattic.com";
+            var profile = await Gravatar.GetUserProfile(validEmail);
+            Assert.NotNull(profile);
+            Assert.NotNull(profile.DisplayName);
+            Assert.NotNull(profile.Id);
+        }
+
+        [Fact]
+        public async Task GetUserProfile_InvaliddProfile_ShouldReturnNull()
+        {
+            var notValidEmail = "notvalid";
+            var profile = await Gravatar.GetUserProfile(notValidEmail);
+            Assert.Null(profile);
+        }
+    }
+}
--- Todo/Services/Gravatar.cs
+++ Todo/Services/Gravatar.cs
@@ -1,10 +1,24 @@
-﻿using System.Security.Cryptography;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Todo.Services
 {
     public static class Gravatar
     {
+        private static HttpClient httpClient;
+
+        static Gravatar()
+        {
+            httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(3);
+        }
+
         public static string GetHash(string emailAddress)
         {
             using (var md5 = MD5.Create())
@@ -19,6 +33,49 @@ namespace Todo.Services
                 }
                 return builder.ToString().ToLowerInvariant();
             }
+        }
+
+        public static async Task<GravatarProfile> GetUserProfile(string emailAddress)
+        {
+            var hash = GetHash(emailAddress);
+            var profileUrl = $"https://www.gravatar.com/{hash}.json";
+            GravatarProfileResponse response = null;
+
+            try
+            {
+                var requestMessage = new HttpRequestMessage(HttpMethod.Get, profileUrl);
+                requestMessage.Headers.UserAgent.Add(new ProductInfoHeaderValue("ToDoListApp", "1.0"));
+                var responseMessage = await httpClient.SendAsync(requestMessage);
+                response = JsonConvert.DeserializeObject<GravatarProfileResponse>(await responseMessage.Content.ReadAsStringAsync());
+            }
+            catch (TaskCanceledException) { }
+            catch (OperationCanceledException) { }
+            catch (JsonSerializationException) { }
+            catch (Exception)
+            {
+                throw;
+            }
+                        
+            if (response?.Entry?.Count > 0)
+            {
+                return response.Entry[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public class GravatarProfileResponse
+        {
+            public List<GravatarProfile> Entry { get; set; }
+        }
+
+        public class GravatarProfile
+        {
+            public string DisplayName { get; set; }
+            public string PreferredUsername { get; set; }
+            public string Id { get; set; }
         }
     }
 }
